f78390c6393d45cfdfbdf11399f0264eeb271abb
Zcamt <Lukas.agerschou.Jensen@gmail.com>
2022-06-23 17:15:29 +0200

    Started work on settings menu

--- src/main/java/me/Zcamt/zgangs/guis/PlayerSettingsGui.java
+++ src/main/java/me/Zcamt/zgangs/guis/PlayerSettingsGui.java
@@ -20,13 +20,14 @@ public class PlayerSettingsGui extends GUI {
     private final GangPlayerManager gangPlayerManager = ZGangs.getGangPlayerManager();
 
     protected PlayerSettingsGui(Player player, Gang playerGang) {
-        super(54, "");
+        super(54, "&a&lPersonlige indstillinger");
         generateGuiBorder();
         this.player = player;
         this.gang = playerGang;
         this.gangPlayer = gangPlayerManager.findById(player.getUniqueId());
 
-
+        //Notifikations menu
+        //Forlad bande
         setItem(49, new ItemCreator(Material.REDSTONE_TORCH).setName("&cTilbage").make());
     }
 
@@ -40,6 +41,47 @@ public class PlayerSettingsGui extends GUI {
                 player.closeInventory();
                 MainGui mainGui = new MainGui(player, gang);
                 mainGui.openTo(player);
+            }
+            case PAPER -> {
+                PlayerNotificationsGui playerNotificationsGui = new PlayerNotificationsGui(player, gang);
+                playerNotificationsGui.openTo(player);
+            }
+        }
+    }
+}
+
+class PlayerNotificationsGui extends GUI {
+    private final Player player;
+    private final Gang gang;
+    private final GangPlayer gangPlayer;
+    private final GangPlayerManager gangPlayerManager = ZGangs.getGangPlayerManager();
+
+    protected PlayerNotificationsGui(Player player, Gang playerGang) {
+        super(54, "&a&lBesked indstillinger");
+        generateGuiBorder();
+        this.player = player;
+        this.gang = playerGang;
+        this.gangPlayer = gangPlayerManager.findById(player.getUniqueId());
+
+        //Gang chat
+        //Ally chat
+        //Member connect
+        //Member disconnect
+        //Ally connect
+        //Ally disconnect
+        setItem(49, new ItemCreator(Material.REDSTONE_TORCH).setName("&cTilbage").make());
+    }
+
+    @Override
+    public void onClick(InventoryClickEvent event) {
+        ItemStack clickedItem = event.getCurrentItem();
+        if(clickedItem == null) return;
+        if(!gangPlayer.isInGang()) return;
+        switch (clickedItem.getType()) {
+            case BARRIER -> {
+                player.closeInventory();
+                PlayerSettingsGui playerSettingsGui = new PlayerSettingsGui(player, gang);
+                playerSettingsGui.openTo(player);
             }
         }
     }
