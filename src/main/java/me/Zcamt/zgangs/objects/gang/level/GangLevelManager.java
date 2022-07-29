32500b2f25138e8199e7e36c44771b25043fc367
Zcamt <Lukas.agerschou.Jensen@gmail.com>
2022-07-29 21:16:21 +0200

    Added up to 5 levels

--- src/main/java/me/Zcamt/zgangs/objects/gang/level/GangLevelManager.java
+++ src/main/java/me/Zcamt/zgangs/objects/gang/level/GangLevelManager.java
@@ -22,12 +22,55 @@ public class GangLevelManager {
                 2, 4, 2,
                 95, 100, false, false, false,
                 1, Arrays.asList(
-                        "&6- &fKøb adgang op til 4 medlemmer",
-                        "&6- &fKøb adgang op til 2 allierede",
-                        "&6- &fNedset bandeskade til 95%"),
+                "&6- &fKøb adgang op til 4 medlemmer",
+                "&6- &fKøb adgang op til 2 allierede",
+                "&6- &fKøb adgang til 95% bandeskade"),
                 new GangLevelRequirements(
                         new GangLevelRequirement(GangLevelRequirementType.BANK_BALANCE, 5000),
                         new GangLevelRequirement(GangLevelRequirementType.MEMBER_COUNT, 2)
+                ))
+        );
+
+        gangLevels.put(3, new GangLevel(
+                3, 4, 2,
+                90, 95, true, false, false,
+                2, Arrays.asList(
+                "&6- &fFå adgang til 2 MOTD-linje i alt",
+                "&6- &fKøb adgang til 95% allyskade",
+                "&6- &fKøb adgang til 90% bandeskade",
+                "&6- &fKøb adgang til bandeområde i C"),
+                new GangLevelRequirements(
+                        new GangLevelRequirement(GangLevelRequirementType.BANK_BALANCE, 15000),
+                        new GangLevelRequirement(GangLevelRequirementType.MEMBER_COUNT, 3),
+                        new GangLevelRequirement(GangLevelRequirementType.ALLY_COUNT, 1)
+                ))
+        );
+
+        gangLevels.put(4, new GangLevel(
+                4, 5, 3,
+                90, 95, true, false, false,
+                3, Arrays.asList(
+                "&6- &fFå adgang til 3 MOTD-linje i alt",
+                "&6- &fKøb adgang op til 5 medlemmer",
+                "&6- &fKøb adgang op til 3 allierede"),
+                new GangLevelRequirements(
+                        new GangLevelRequirement(GangLevelRequirementType.BANK_BALANCE, 35000),
+                        new GangLevelRequirement(GangLevelRequirementType.DELIVER_BREAD, 64),
+                        new GangLevelRequirement(GangLevelRequirementType.DELIVER_CIGS, 1)
+                ))
+        );
+
+        gangLevels.put(5, new GangLevel(
+                5, 5, 3,
+                85, 85, true, true, false,
+                3, Arrays.asList(
+                "&6- &fKøb adgang til 85% allyskade",
+                "&6- &fKøb adgang til 85% bandeskade",
+                "&6- &fKøb adgang til bandeområde i B"),
+                new GangLevelRequirements(
+                        new GangLevelRequirement(GangLevelRequirementType.BANK_BALANCE, 75000),
+                        new GangLevelRequirement(GangLevelRequirementType.DELIVER_BREAD, 256),
+                        new GangLevelRequirement(GangLevelRequirementType.DELIVER_CIGS, 16)
                 ))
         );
     }
