8ff25fad090ebd747fa430a96e373b38b1d1992e
Zcamt <Lukas.agerschou.Jensen@gmail.com>
2022-07-29 21:11:05 +0200

    Added the initial few placeholders

--- src/main/java/me/Zcamt/zgangs/api/Placeholders.java
+++ src/main/java/me/Zcamt/zgangs/api/Placeholders.java
@@ -1,12 +1,18 @@
 package me.Zcamt.zgangs.api;
 
 import me.Zcamt.zgangs.ZGangs;
+import me.Zcamt.zgangs.objects.gang.Gang;
+import me.Zcamt.zgangs.objects.gang.GangManager;
+import me.Zcamt.zgangs.objects.gangplayer.GangPlayer;
+import me.Zcamt.zgangs.objects.gangplayer.GangPlayerManager;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.entity.Player;
 
 public class Placeholders extends PlaceholderExpansion {
 
     private final ZGangs plugin;
+    private final GangManager gangManager = ZGangs.getGangManager();
+    private final GangPlayerManager gangPlayerManager = ZGangs.getGangPlayerManager();
 
     public Placeholders(ZGangs plugin) {
         this.plugin = plugin;
@@ -45,10 +51,48 @@ public class Placeholders extends PlaceholderExpansion {
             return null;
         }
 
-        /* %zgangs_gang_name%
-        if(identifier.equalsIgnoreCase("zgangs_gang_name")){
 
-        }*/
+        //%zgangs_gang_name%
+        if(identifier.equalsIgnoreCase("zgangs_gang_name")){
+            GangPlayer gangPlayer = gangPlayerManager.findById(player.getUniqueId());
+            if(gangPlayer == null) return "Ingen bande";
+
+            Gang playerGang = gangManager.findById(gangPlayer.getGangUUID());
+            if(playerGang == null) return "Ingen bande";
+
+            return playerGang.getName();
+        }
+
+        //%zgangs_gang_bank%
+        if(identifier.equalsIgnoreCase("zgangs_gang_bank")){
+            GangPlayer gangPlayer = gangPlayerManager.findById(player.getUniqueId());
+            if(gangPlayer == null) return "Ingen bande";
+
+            Gang playerGang = gangManager.findById(gangPlayer.getGangUUID());
+            if(playerGang == null) return "Ingen bande";
+
+            return String.valueOf(playerGang.getBank());
+        }
+
+        //%zgangs_gang_online%
+        if(identifier.equalsIgnoreCase("zgangs_gang_online")){
+            GangPlayer gangPlayer = gangPlayerManager.findById(player.getUniqueId());
+            if(gangPlayer == null) return String.valueOf(0);
+
+            Gang playerGang = gangManager.findById(gangPlayer.getGangUUID());
+            if(playerGang == null) return String.valueOf(0);
+
+            return String.valueOf(playerGang.getGangMembers().getMemberList().size());
+        }
+
+        //%zgangs_gangplayer_rank%
+        if(identifier.equalsIgnoreCase("zgangs_gangplayer_rank")){
+            GangPlayer gangPlayer = gangPlayerManager.findById(player.getUniqueId());
+            if(gangPlayer == null) return "Ingen rang";
+            if(!gangPlayer.isInGang()) return "Ingen bande";
+
+            return gangPlayer.getGangRank().getName();
+        }
 
         return null;
     }
