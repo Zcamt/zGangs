5f734227120a6217ac65448303edfb2104801d78
Zcamt <Lukas.agerschou.Jensen@gmail.com>
2022-07-30 16:31:42 +0200

    Players are now removed from the system as waiting for a confirmation when they close the inventory.

--- src/main/java/me/Zcamt/zgangs/listeners/InventoryListener.java
+++ src/main/java/me/Zcamt/zgangs/listeners/InventoryListener.java
@@ -1,15 +1,21 @@
 package me.Zcamt.zgangs.listeners;
 
+import me.Zcamt.zgangs.ZGangs;
+import me.Zcamt.zgangs.confirmaction.ConfirmActionManager;
 import me.Zcamt.zgangs.guis.GUI;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class InventoryListener implements Listener {
+
+    private final ConfirmActionManager confirmActionManager = ZGangs.getConfirmActionManager();
 
     @EventHandler
     public void onClick(InventoryClickEvent event) {
@@ -21,6 +27,15 @@ public class InventoryListener implements Listener {
         if(inventory.getHolder() instanceof GUI gui) {
             event.setCancelled(true);
             gui.onClick(event);
+        }
+    }
+
+
+    @EventHandler
+    public void onClose(InventoryCloseEvent event) {
+        Player player = (Player) event.getPlayer();
+        if(confirmActionManager.awaitsConfirmationFrom(player)) {
+            confirmActionManager.removeFromConfirmationMap(player);
         }
     }
 
