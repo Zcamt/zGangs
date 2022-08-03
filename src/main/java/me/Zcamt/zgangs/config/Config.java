6795cc1a50cd3f1f239f91e92779773a9858e16a
Zcamt <Lukas.agerschou.Jensen@gmail.com>
2022-08-03 15:29:03 +0200

    Added extra config values

--- src/main/java/me/Zcamt/zgangs/config/Config.java
+++ src/main/java/me/Zcamt/zgangs/config/Config.java
@@ -17,6 +17,13 @@ public class Config {
     public static int maxNameLength, minNameLength;
     public static int transferGangCost, createGangCost, gangRenameCost;
 
+    public static int cigPrice;
+    public static int gangAreaCPrice, gangAreaBPrice, gangAreaAPrice;
+    public static int gangDamageUpgradeStartPrice, gangDamageUpgradeIncreaseCoefficient;
+    public static int allyDamageUpgradeStartPrice, allyDamageUpgradeIncreaseCoefficient;
+    public static int memberUpgradeStartPrice, memberUpgradeIncreaseCoefficient;
+    public static int allyUpgradeStartPrice, allyUpgradeIncreaseCoefficient;
+
     public static ImmutableList<String> bannedWords;
 
     public static void reload() {
@@ -32,6 +39,24 @@ public class Config {
         transferGangCost = config.getInt("gangs.gang-transfer-cost");
         createGangCost = config.getInt("gangs.gang-create-cost");
         gangRenameCost = config.getInt("gangs.gang-rename-cost");
+
+        cigPrice = config.getInt("gangs.shop.cig-price");
+
+        gangAreaCPrice = config.getInt("gangs.shop.gang-area-c-price");
+        gangAreaBPrice = config.getInt("gangs.shop.gang-area-b-price");
+        gangAreaAPrice = config.getInt("gangs.shop.gang-area-a-price");
+
+        gangDamageUpgradeStartPrice = config.getInt("gangs.shop.gang-damage.start-price");
+        gangDamageUpgradeIncreaseCoefficient = config.getInt("gangs.shop.gang-damage.pr-upgrade-increase-coefficient");
+
+        allyDamageUpgradeStartPrice = config.getInt("gangs.shop.ally-damage.start-price");
+        allyDamageUpgradeIncreaseCoefficient = config.getInt("gangs.shop.ally-damage.pr-upgrade-increase-coefficient");
+
+        memberUpgradeStartPrice = config.getInt("gangs.shop.member-count.start-price");
+        memberUpgradeIncreaseCoefficient = config.getInt("gangs.shop.member-count.pr-upgrade-increase-coefficient");
+
+        allyUpgradeStartPrice = config.getInt("gangs.shop.ally-count.start-price");
+        allyUpgradeIncreaseCoefficient = config.getInt("gangs.shop.ally-count.pr-upgrade-increase-coefficient");
 
         bannedWords = ImmutableList.copyOf(config.getStringList("gangs.banned-words"));
     }
